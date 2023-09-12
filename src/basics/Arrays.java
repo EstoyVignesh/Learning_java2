7fb22dc165d74e479dadf7a6744773bf3d8e8b9a
Vignesh <vignesh000vky@gmail.com>
2023-09-12 22:15:52 +0530

    Methods and Arrays are added

---
+++ src/basics/Arrays.java
@@ -0,0 +1,29 @@
+package basics;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] num;
+		num = new int[4];
+		num[0]=100;
+		num[1]=150;
+		num[2]=200;
+		num[3]=250;
+		
+		System.out.println("The Value of num is "+num[2]);
+		
+		
+		// Second Method
+		int[] num1= {25,35,45,55,65};
+		System.out.println("The VAlue is "+num1.length);
+		
+		//Third Way
+		
+		int num2[]= new int[4];
+		int num3[]= {1,2,3,4,5};
+		System.out.println("THe value is "+num3);
+	}
+
+}
