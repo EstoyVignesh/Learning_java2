a4273d6497e8edbb64dbceaf6d3b5d216a6a5f56
Vignesh <vignesh000vky@gmail.com>
2023-10-11 16:28:39 +0530

    New Changes in Task 13

--- src/test/java/task13_apache_poi/ReadSheet1_FileOperation.java
+++ src/test/java/task13_apache_poi/ReadSheet1_FileOperation.java
@@ -1,5 +1,7 @@
 package task13_apache_poi;
 
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadSheet1_FileOperation {
@@ -10,8 +12,46 @@ public class ReadSheet1_FileOperation {
 		try
 		{
 		
-		XSSFWorkbook workbook= new XSSFWorkbook("C:\\Users\\arvin\\eclipse-workspace\\selenium_learning\\src\\test\\resources\\exceldata.xlsx");
+			// to Mention the file path, use this below 
+			XSSFWorkbook workbook= new XSSFWorkbook("C:\\Users\\arvin\\eclipse-workspace\\Mavenone\\src\\test\\resources\\Sheet1.xlsx");
+			
+			//To read the index from 0th Row and 0th Column
+			//XSSFSheet sheet= workbook.getSheet("data");
+			XSSFSheet sheet= workbook.getSheetAt(0);
+			
+			//to go to first row
+			XSSFRow firstRow= sheet.getRow(0);
+			
+			//To get the Total No. of Rows
+			int rows= sheet.getPhysicalNumberOfRows(); // no of rows (5)
+			
+			//To get the Total No. of Columns
+			int cells= firstRow.getPhysicalNumberOfCells(); //no of columns(3)
+			
+			
+			//To start iteration Start from Row 0
+			for(int i=1; i<rows;i++)
 
+			{
+				
+				//To start iteration Start from Column 0
+				for(int j=0; j<cells;j++)
+				{
+					
+					//To get the value of Row, Column and the String Value
+				//	String cellValue=sheet.getRow(i).getCell(j).getStringCellValue();
+					String cellValue=sheet.getRow(i).getCell(j).getStringCellValue();
+
+					
+					//To write to an excel on a particular row
+					//sheet.createRow(5).createCell(0).setCellValue("writing");
+					
+					System.out.println ("Cell value-->"+cellValue);
+					
+				} 
+			}
+			
+			
 		}
 		catch(Exception ex)
 		{
